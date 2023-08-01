f9dcd808309e97996fc1c04cdb30c4bdbc9e7e67
Nemo <truongng201@gmail.com>
2023-08-01 22:26:48 +0700

    minor update

--- server/pkg/routes/routes.go
+++ server/pkg/routes/routes.go
@@ -2,8 +2,9 @@ package routes
 
 import (
 	"runtime/debug"
-        "net/http"
-	
+    "html/template"
+    "io"
+    
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -24,16 +25,33 @@ var commit_hash = func() string {
     return ""
 }()
 
+type TemplateRenderer struct {
+	templates *template.Template
+}
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func Routes(e *echo.Echo) *echo.Echo {
 	e.Use(middleware.Logger())
 
 	e.GET("/health", func(c echo.Context) error {
 		return c.JSON(200, &HealthCheck{
-      Status: "Ok",
-      Version: commit_hash,
-    })
+            Status: "Ok",
+            Version: commit_hash,
+        })
 	})
-	
-	e.File("/", "../../../client/out/index.html")
+    renderer := &TemplateRenderer{
+        templates: template.Must(template.ParseGlob("html/*.html")),
+    }
+	e.Renderer = renderer
+	e.GET("/", func(c echo.Context) error {
+        return c.Render(200, "../../../client/out/index.html", "")
+    })
 	return e
 }
