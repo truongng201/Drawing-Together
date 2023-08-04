37eaf52d00f23283fb466568287163a41a81373a
NemoPC <truongng201@gmail.com>
2023-08-04 15:04:46 +0700

    fix ci cd

--- scripts/git-check.sh
+++ scripts/git-check.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Define the folder you want to monitor
-FOLDER_TO_MONITOR="../server"
+FOLDER_TO_MONITOR=$1
 
 # Go to the repository root directory
 REPO_ROOT=$(git rev-parse --show-toplevel)
@@ -10,13 +9,12 @@ cd "$REPO_ROOT" || exit 1
 # Get the current folder name
 CURRENT_FOLDER_NAME=$(basename "$FOLDER_TO_MONITOR")
 
-# Check for changes in the folder name
+  # Check for changes in the folder name
 CHANGED=$(git diff --name-only HEAD^ HEAD | grep "$CURRENT_FOLDER_NAME")
 
 if [[ -n "$CHANGED" ]]; then
-  echo "Folder $CURRENT_FOLDER_NAME has changed in the repository!"
-  exit 1
+  echo "$FOLDER_TO_MONITOR changed"
+  exit 0
 else
-  echo "Folder $CURRENT_FOLDER_NAME hasn't changed in the repository!"
   exit 0
 fi
