c1a5566688cdd2da81d0b07d3b750f3532d5aeff
Nemo <truongng201@gmail.com>
2023-08-11 20:46:34 +0700

    minor update

--- client/src/app/components/SideNavbar.js
+++ client/src/app/components/SideNavbar.js
@@ -2,7 +2,6 @@ export default function SideNavbar({ children }) {
     return (
         <div className="sidenavbar-container">
             <div className="sidenavbar">
-                hi
                 <nav>
                     <ul> Home </ul>
                     <ul> Monitor </ul>
