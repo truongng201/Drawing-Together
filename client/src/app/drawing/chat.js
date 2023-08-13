f08ded0dc1468938c0ac505f37fae2b0676ab00f
NemoPC <truongng201@gmail.com>
2023-08-13 16:54:55 +0700

    update UI chat

--- client/src/app/drawing/chat.js
+++ client/src/app/drawing/chat.js
@@ -3,7 +3,20 @@ import './chat.css';
 export default function Chat() {
     return (
         <div className="chat">
-            
+            <div className='left-chat chat-container'>
+                <div className='chat-messages'>
+                    Text
+
+
+                </div>
+                <input type="text" className="chat-input" placeholder='Guess here' />
+            </div>
+            <div className='right-chat chat-container'>
+                <div className='chat-messages'>
+                    Text
+                </div>
+                <input type="text" className="chat-input" placeholder='Chat here' />
+            </div>
         </div>
     )
 }
