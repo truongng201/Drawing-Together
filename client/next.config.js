df20f82e97052bc2d42c985740bdd09c95359862
NemoPC <truongng201@gmail.com>
2023-08-17 13:59:33 +0700

    optimize docker image file for client

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
     env: {
         NEXT_PUBLIC_SOCKET_URI: process.env.NEXT_PUBLIC_SOCKET_URI,
     },
+    output: "standalone",
+    reactStrictMode: true,
 };
 
 module.exports = nextConfig;
