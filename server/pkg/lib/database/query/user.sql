7246edc039cc1102cd5206b6afae36179d769cb8
NemoPC <truongng201@gmail.com>
2023-08-19 17:11:26 +0700

    minor update

--- server/pkg/lib/database/query/user.sql
+++ server/pkg/lib/database/query/user.sql
@@ -1,7 +1,11 @@
 CREATE TABLE IF NOT EXISTS "user" (
     "id"            UUID PRIMARY KEY,
+    "username"      TEXT NOT NULL,
     "email"         TEXT NOT NULL UNIQUE,
     "password_hash" TEXT NOT NULL,
     "created_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "updated_at"    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at"    TIMESTAMP NULL DEFAULT NULL,
+    "avatar_url"    TEXT NULL DEFAULT NULL,
+    
 );
