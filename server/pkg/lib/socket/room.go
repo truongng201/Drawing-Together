2decfcf82037a6d37c7fec60911e9e035afcdbfb
NemoPC <truongng201@gmail.com>
2023-08-21 12:14:31 +0700

    minor update

--- server/pkg/lib/socket/room.go
+++ server/pkg/lib/socket/room.go
@@ -1,8 +1,6 @@
 package socket
 
 import (
-	"encoding/json"
-
 	"github.com/labstack/gommon/log"
 
 	"github.com/google/uuid"
@@ -11,7 +9,7 @@ import (
 // Room is a pool of connections
 type Room struct {
 	// Registered connections.
-	RoomID	  	string		`json:"room_id"`
+	RoomID	  	string			`json:"room_id"`
 	Private		bool			`json:"private"`
 	MaxPlayers	int				`json:"max_players"`
 	Register    chan *Client
@@ -40,7 +38,7 @@ func (room *Room) Start() {
 		case client := <-room.UnRegister:
 			room.unregisterClient(client)
 		case message := <-room.Broadcast:	
-			room.broadcastMessage(message)
+			room.broadcastMessage(message.encode())
 		}
 	}
 }
@@ -64,15 +62,12 @@ func (room *Room) unregisterClient(client *Client) {
 	log.Info("A user left the chat")
 }
 
-func (room *Room) broadcastMessage(message Message) {
-	log.Info("room.broadcastMessage.len_rooom: ", len(room.Clients))
+func (room *Room) broadcastMessage(message []byte) {
+	log.Info("Broadcasting message to all clients in room")
+	log.Info("Size of Connection Room: ", len(room.Clients))
+	log.Info("Room ID: ", room.RoomID)
 	for client := range room.Clients {
-		log.Info("room.broadcastMessage.msg", client)
-		msg, err := json.Marshal(message)
-		if err != nil{
-			log.Error("room.broadcastMessage.err", err)
-		}
-		client.send <- msg
+		client.send <- message
 	}
 }
 
